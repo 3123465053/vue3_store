01a72b59f4dca7e9fe2975e0f916a5d87d2395fd
3123465053 <3123465053@qq.com>
2024-11-14 10:22:20 +0800

    定制主题颜色 sacc

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+   <el-button type="primary">安修</el-button>
+
+</template>
+
+<style scoped>
+</style>
---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+//elementPlus 按需导入 1
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+
+    //2.按需导入步骤2
+    AutoImport({
+      resolvers:[ElementPlusResolver()]
+    }),
+    Components({
+      //1.配置element Plus采用sass样式配色系统
+      resolvers:[ElementPlusResolver({importStyle:"sass"})]
+    }),
+
+
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  css:{
+    preprocessorOptions:{
+      scss:{
+        //2.自动导入定制化样式文件进行样式覆盖
+        additionalData:`
+        @use "@/styles/element/index.scss" as *;
+        `
+      }
+    }
+  }
+})
